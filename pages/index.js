89aef706cda9e381887848a0f30c5b6648037b0a
Pierre Luce <pluce19@gmail.com>
2021-03-15 20:07:14 +0100

    better search behaviour

--- pages/index.js
+++ pages/index.js
@@ -1,27 +1,36 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
-import { Container, Row, Col, FormInput, Button, Card, CardBody, CardTitle, CardFooter } from "shards-react";
+import { Container, Row, Col, FormInput, Form, Button, Card, CardBody, CardTitle, CardFooter, Progress } from "shards-react";
 import { useState } from 'react';
 
 export default function Home() {
   const [results, setResults] = useState([])
   const [keywords, setKeywords] = useState(null)
+  const [searching, setSearching] = useState(false)
   const handleChange = (e) => {
     setKeywords(e.target.value)
   }
   const doSearch = async (e) => {
+    e.preventDefault();
+    setSearching(true)
     const api_search = await fetch(`/api/search?criteria=${keywords}`)
     const content = await api_search.json()
     setResults(content.hits.hits.map(hit => ({file: hit._source.file, page: hit._source.page, content: hit.highlight.content})))
+    setSearching(false)
   }
   return (
     <Container>
       <Row>
         <Col sm={{ size: 8, order: 2, offset: 2 }}>
-          <FormInput size="lg" placeholder="Large input" onChange={handleChange}/>
-          <Button block onClick={doSearch}>Rechercher</Button>
+          <Form onSubmit={doSearch}>
+            <FormInput size="lg" placeholder="Entrez votre recherche et cliquez sur Rechercher" onChange={handleChange}/>
+            <Button action="submit" block>Rechercher</Button>
+          </Form>
         </Col>
       </Row>
+      { searching && <Row>
+        <Col sm={{ size: 10, offset: 1 }}><Progress theme="primary" animated={true} stripped={true} value="100"/></Col>
+      </Row> }
         { results.map( res => 
       <Row>
         <Col sm={{ size: 10, offset: 1 }}>
